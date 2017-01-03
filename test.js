bd1f5ca87003766e9ac4d6e7fbaa95264695e034
Randi Vertongen <r0460870@student.thomasmore.be>
2017-01-03 02:13:50 +0100

    Blijkbaar zat iik verkeerde test.js te committen --> stond op bureablad. Dus nu update dan --> Test wordt normaal gezien main js file

--- test.js
+++ test.js
@@ -1,14 +1,21 @@
+var express = require("express");
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/test');
+var dalLocations = require("./Locationst.js")
+var dalAanwezigheden= require("./Aanwezighedenst.js")
+// var daldroneid = require (./Droneid.js") Laten we nog even achterwege.
 
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  var kittySchema = mongoose.Schema({
-      name: String
-  });
-  var Kitten = mongoose.model('Kitten', kittySchema);
 
-  var silence = new Kitten({ name: 'Silence' });
-console.log(silence.name); // 'Silence'
+var app = express();
+// https://expressjs.com/en/guide/routing.html
+// express is een manier om http requests te sturen via NODEJS
+// Minimalistisch framework dat veel gebruikt wordt.
+
+app.get("/locations",function( req,res){
+dalLocations.listAllLocations(function (err,Locations){
+  if (err){
+    throw err;
+  }
+  res.send(locations);
+});
 });
