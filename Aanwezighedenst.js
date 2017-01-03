023a2092143f00191604bc853a378afdd6c85a75
Randi Vertongen <r0460870@student.thomasmore.be>
2017-01-03 03:40:08 +0100

    Aanwezigheden basisfile klaargemaakt voor mongoose.

--- Aanwezighedenst.js
+++ Aanwezighedenst.js
@@ -1,6 +1,29 @@
+
+var mongoose = require("mongoose");
+var AanwezigheidSchema = mongoose.Schema({
+  aanwid:{
+    type: Number
+  },
+naam:{
+type: String
+}
+
+
+
+// Schema & Model http://mongoosejs.com/docs/guide.html
+});
+var aanwezigheid = mongoose.model('aanwezigheden',AanwezigheidSchema);
+
+
+
+
+
+
+
+
 module.exports =  {
   Aanwezigheden : {},
-  
+
   saveAanwezigheid : function(Aanwezigheid){
     this.Aanwezigheden[Aanwezigheid.id]= Aanwezigheid;
   },
