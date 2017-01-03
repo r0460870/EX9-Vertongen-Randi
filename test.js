5efacae6750990f9946c1b30ab6ead6d68e44486
Randi Vertongen <r0460870@student.thomasmore.be>
2017-01-03 03:08:17 +0100

    effe vergeten committen. Staan veel typfouten in, ben nog bezig met location

--- test.js
+++ test.js
@@ -11,6 +11,8 @@ var app = express();
 // https://expressjs.com/en/guide/routing.html
 // express is een manier om http requests te sturen via NODEJS
 // Minimalistisch framework dat veel gebruikt wordt.
+
+
 app.use(parser.json());
 app.get("/locations",function( req,res){
     dalLocations.listAllLocations(function (err,Locations){
@@ -23,5 +25,28 @@ app.get("/locations",function( req,res){
   // response --> alle locaties worden gestuurd via send kunnen ook bijvoorbeeld console.loges meegegeven worden
     });
 });
+
+var Location = function (locid,naam,stad){
+  this.locid = locid;
+  this.naam = naam;
+  this.stad = stad;
+};
+app.post("/locations",function(req,res){
+//req.body holds parameters that are sent up from the client as part of a POST request
+//hieronder dus.
+    var Locatie = new Location(req.body.locid,req.body.naam,req.body.stad);
+    dalLocations.createLocation(locatie,function(err,locatie){
+        if (err){
+        console.log(err);
+        }
+    res.send(locatie);
+// creeren van nieuwe locatie via post, puit voor updates.
+    });
+});
+app.put("/locations/:locid",function(req,res){
+  // verdergaand op locid omdat hierop check moet uitgevoerd worden.
+    var Locatie = new Location(req.body.locid,req.body.naam,req.body.stad);
+    dalLocations.updateLocation(req.params.locid
+}
 app.listen(3000);
 console.log("start");
